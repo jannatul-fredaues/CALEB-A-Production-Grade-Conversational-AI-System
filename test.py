f5e1ca89e8c7df258a76b4007fe5cf285aad2c41
Jannatul Ferdaues <207971567+jannatul-fredaues@users.noreply.github.com>
2026-03-19 10:26:54 +0600

    Add files via upload

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from groq import Groq
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # 👈 loads .env file
+load_dotenv()  # loads .env file
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
